412e0e4e460e5591d879515bc1dadb954a674011
Hu4k3n <arjun.syam23@gmail.com>
2025-05-26 01:56:43 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoadingPage from './packages/LoadingPage';
 import MainMenu from './packages/MainMenu';
-import GodotGame from './packages/GodotGame';
+import GodotGame from './packages/GameInit/GodotGame';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
